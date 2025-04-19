22fa59d54be1472f5b05f19ad6f70124f3236c3a
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2025-04-19 20:03:38 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int num[9];
+	for (int i = 0; i < 9; i++) {
+		cin >> num[i];
+	}
+
+	int maxValue = -1;
+	int maxValueIndex;
+	for (int i = 0; i < 9; i++) {
+		if (num[i] > maxValue) {
+			maxValue = num[i];
+			maxValueIndex = i;
+		}
+	}
+
+	cout << maxValue << "\n" << maxValueIndex + 1;
+
+	return 0;
+}
