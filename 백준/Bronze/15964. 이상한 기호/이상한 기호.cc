f3dc90549d451fb2d5233d3c20d712d3022e5fb0
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2025-04-21 00:18:41 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -1,15 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int solution(int a, int b) {
-	return (a + b) * (a - b);
-}
-
 int main() {
-	int n1, n2;
-	cin >> n1 >> n2;
-
-	cout << solution(n1, n2) << "\n";
-
-	return 0;
+    long long a, b;
+    cin>>a>>b;
+    cout<<(long long)(a+b)*(a-b);
+    return 0;
 }
